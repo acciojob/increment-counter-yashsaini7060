96158f9ddb62ffe5747809d78763a2fa8c3adcf6
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 22:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 
-function incrementCounter() {
-  var counter = document.getElementById("counter");
-  var currentValue = Number(counter.innerHTML);
-  // alert("Current value: " + currentValue);
-  counter.innerHTML = currentValue + 1;
-}
+document.getElementById("incrementBtn").addEventListener("click", function() {
+	var value = parseInt(document.getElementById("counter").innerHTML);
+	value+=1;
+	// alert(value)	
+	document.getElementById("counter").innerHTML = value;
+});
